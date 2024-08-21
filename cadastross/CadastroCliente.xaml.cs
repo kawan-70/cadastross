91c6edbaf7b7a19bb2574fd9e3ccef8624f8a40f
cadastross <kauan220656@gmail.com>
2024-08-21 11:52:02 -0300

    termine a tela cadastro do cliente

--- cadastross/CadastroCliente.xaml.cs
+++ cadastross/CadastroCliente.xaml.cs
@@ -3,6 +3,11 @@ using Microsoft.Maui.Controls;
 namespace cadastross
 {
     public partial class CadastroCliente : ContentPage
+
+//    public Cliente cliente { get; set; }
+//  Controles.ClienteControle clienteControle = new Controles.ClienteControle();
+//  Controles.EstadoControle estadoControle = new Controles.EstadoControle();
+ 
     {
         public CadastroCliente()
         {
